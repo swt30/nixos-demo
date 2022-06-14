e84aa1c574eeed3525bc75bedc78700a9a727561
Scott Thomas <scott@englishlanguageitutoring.com>
2022-06-14 13:03:15 +0100

    Use functions in install script

--- install-nixos.sh
+++ install-nixos.sh
@@ -5,17 +5,27 @@
 # It uses a BIOS/GRUB install, no swap space.
 # Run it as root on the VM.
 
-# Create partitions
-parted /dev/sda -- mklabel msdos
-parted /dev/sda -- mkpart primary 1MiB 100%
+create_partitions () {
+  parted /dev/sda -- mklabel msdos
+  parted /dev/sda -- mkpart primary 1MiB 100%
+}
 
-# Create and mount file system
-mkfs.ext4 -L nixos /dev/sda1
-mount /dev/disk/by-label/nixos /mnt
+create_and_mount_file_system () {
+  mkfs.ext4 -L nixos /dev/sda1
+  mount /dev/disk/by-label/nixos /mnt
+}
 
 # Set nixos configuration
-nixos-generate-config --root /mnt
-cp configuration.nix /mnt/etc/nixos/configuration.nix
+configure_nixos () {
+  nixos-generate-config --root /mnt
+  cp ./configuration.nix /mnt/etc/nixos/configuration.nix
+}
 
-# Install
-nixos-install
+install_nixos () {
+  nixos-install --no-root-password
+}
+
+create_partitions
+create_and_mount_file_system
+configure_nixos
+install_nixos
