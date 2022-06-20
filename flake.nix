503de6b4852d4c6487b3ebf6a91a85cbf680999b
Scott Thomas <swt30@users.noreply.github.com>
2022-06-20 23:10:23 +0100

    Add a flake

---
+++ flake.nix
@@ -0,0 +1,33 @@
+{
+  description = "A barebones flake for a NixOS installation demo";
+
+  inputs.nixpkgs.url = "nixpkgs/nixos-22.05";
+
+  outputs = { self, nixpkgs }: {
+    nixosConfigurations.nixos = nixpkgs.lib.nixosSystem {
+      system = "x86_64-linux";
+      modules = [
+        ./hardware-configuration.nix
+        ({ pkgs, ... }: {
+          boot.loader.grub.enable = true;
+          boot.loader.grub.version = 2;
+          boot.loader.grub.device = "/dev/vda";
+
+          networking.hostName = "nixos";
+
+          time.timeZone = "Europe/London";
+
+          users.users.nix = {
+            password = "nix";
+            isNormalUser = true;
+            extraGroups = [ "wheel" ];
+          };
+
+          environment.systemPackages = [ pkgs.nano ];
+
+          system.stateVersion = "22.05";
+        })
+      ];
+    };
+  };
+}
