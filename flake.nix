574a26becdb737a61850f777f1962d511b958234
Scott Thomas <swt30@users.noreply.github.com>
2022-06-20 23:18:21 +0100

    Put config nearer the top

--- flake.nix
+++ flake.nix
@@ -3,31 +3,32 @@
 
   inputs.nixpkgs.url = "nixpkgs/nixos-22.05";
 
-  outputs = { self, nixpkgs }: {
-    nixosConfigurations.nixos = nixpkgs.lib.nixosSystem {
-      system = "x86_64-linux";
-      modules = [
-        ./hardware-configuration.nix
-        ({ pkgs, ... }: {
-          boot.loader.grub.enable = true;
-          boot.loader.grub.version = 2;
-          boot.loader.grub.device = "/dev/vda";
+  outputs = { self, nixpkgs }: let
+    system-config = { pkgs, ... }: {
+      boot.loader.grub.enable = true;
+      boot.loader.grub.version = 2;
+      boot.loader.grub.device = "/dev/vda";
 
-          networking.hostName = "nixos";
+      networking.hostName = "nixos";
 
-          time.timeZone = "Europe/London";
+      time.timeZone = "Europe/London";
 
-          users.users.nix = {
-            password = "nix";
-            isNormalUser = true;
-            extraGroups = [ "wheel" ];
-          };
+      users.users.nix = {
+        password = "nix";
+        isNormalUser = true;
+        extraGroups = [ "wheel" ];
+      };
 
-          environment.systemPackages = [ pkgs.nano ];
+      environment.systemPackages = [ pkgs.nano ];
 
-          system.stateVersion = "22.05";
-        })
-      ];
+      system.stateVersion = "22.05";
+    }; in {
+      nixosConfigurations.nixos = nixpkgs.lib.nixosSystem {
+        system = "x86_64-linux";
+        modules = [
+          ./hardware-configuration.nix
+          system-config
+        ];
+      };
     };
-  };
 }
