75acba81d817a5dcdaf6a34472a2e0815ec38d93
Scott Thomas <swt30@users.noreply.github.com>
2022-06-20 23:48:41 +0100

    Comment tweak

--- install-nixos.sh
+++ install-nixos.sh
@@ -3,6 +3,8 @@
 # This simple script is used to demo installing NixOS on a virtual machine.
 # It uses a BIOS/GRUB install with no swap partition.
 # Run it as root on the VM.
+# You might have to change the drive below - for example, using qemu on mac,
+# the disk is called /dev/sda, but under gnome-boxes on linux it is /dev/vda.
 
 create_partitions () {
   parted /dev/sda -- mklabel msdos
